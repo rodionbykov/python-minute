32b6eba3a7b4c36df535d9ca534cfe2e29d7a600
Rodyon Bykov <rbykov@intercars.eu>
2021-11-01 21:02:26 +0100

    Dynamic path segments

--- django-udemy-schwarzmuller/challenges/views.py
+++ django-udemy-schwarzmuller/challenges/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This is our first page")
+def index(request, month):
+    return_text = ""
+    match month:
+        case "january":
+            return_text = "Do not overeat"
+        case "february":
+            return_text = "Get yourself warm"
+        case _:
+            return_text = "No challenge for you"
+    return HttpResponse(return_text)
