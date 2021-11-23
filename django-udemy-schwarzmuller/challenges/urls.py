ba4c6379b33ab4909b09f507b844034b1d406679
Rodion Bykov <rodionbykov@gmail.com>
2021-11-23 23:25:05 +0100

    Template includes and partial includes

--- django-udemy-schwarzmuller/challenges/urls.py
+++ django-udemy-schwarzmuller/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index), # empty root url, list of months
+    path("", views.index, name="index"), # empty root url, list of months
     path("<int:month_number>", views.challenges_by_month_number),
     path("<str:month_name>", views.challenges_by_month_name, name="monthly_challenge")
 ]
