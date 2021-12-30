837e25b4236ee41c08e72c31dc563f762db2c263
Rodion Bykov <rodionbykov@gmail.com>
2021-12-31 00:05:18 +0100

    Page links

--- django-udemy-blog/blog/urls.py
+++ django-udemy-blog/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("posts", views.posts),
+    path("", views.index, name="index"),
+    path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.detail, name="post-detail-page")
 ]
