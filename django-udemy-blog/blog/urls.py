eb2b96afc927212129f54ff70a0880618f686253
Rodion Bykov <rodionbykov@gmail.com>
2022-01-03 23:39:35 +0100

    Dynamic data in templates and posts details page

--- django-udemy-blog/blog/urls.py
+++ django-udemy-blog/blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.detail, name="post-detail-page")
+    path("posts/<slug:slug>", views.details, name="post-details-page")
 ]
--- django-udemy-blog/blog/views.py
+++ django-udemy-blog/blog/views.py
@@ -1,12 +1,37 @@
+from datetime import date
+
 from django.shortcuts import render
+
+arr_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Sam Frodo",
+        "date": date(2021, 12, 30),
+        "title": "Hiking in the mountains",
+        "excerpt": "Aliquam erat volutpat. Curabitur dignissim a sem eu interdum. Vivamus fermentum euismod pulvinar.",
+        "content": """
+            Aenean vel ipsum ac nibh congue luctus. 
+            Sed hendrerit consequat metus ac sagittis. Donec ac risus molestie, condimentum sem sit amet, lobortis eros. Nulla facilisi. Aliquam fermentum pellentesque nunc quis fermentum. Nullam justo augue, viverra sed enim quis, placerat efficitur augue. Aliquam volutpat euismod sapien, quis dignissim purus pharetra vitae. Ut molestie nisi at ultrices aliquam. 
+            Aliquam erat volutpat. Nullam at metus ante. Nam sed maximus sapien, semper consectetur nisi. Sed et accumsan elit. Donec auctor libero eu porttitor viverra. Duis quis eros vitae mi congue pretium at eget nibh. Mauris sed odio sit amet sem luctus pellentesque. Nam faucibus, neque vitae eleifend ultricies, elit felis aliquam nibh, sed sodales sem felis ut dolor.
+        """
+    }
+]
 
 # Create your views here.
 
 def index(request):
-    return render(request, "blog/index.html")
+    return render(request, "blog/index.html", {
+        "posts": arr_posts
+    })
 
 def posts(request):
-    return render(request, "blog/posts.html")
+    return render(request, "blog/posts.html", {
+        "posts": arr_posts
+    })
 
-def detail(request):
-    pass
+def details(request, slug):
+    the_post = next(post for post in arr_posts if post['slug'] == slug)
+    return render(request, "blog/details.html", {
+        "post": the_post
+    })
