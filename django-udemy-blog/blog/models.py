92246187066b95dc696be58e599a184428c6f071
Rodion Bykov <rodionbykov@gmail.com>
2022-01-18 23:52:18 +0100

    Custom models added

--- django-udemy-blog/blog/models.py
+++ django-udemy-blog/blog/models.py
@@ -1,3 +1,16 @@
+from pyexpat import model
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=127)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    likes = models.IntegerField(validators=[MinValueValidator(0)])
+    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE, related_name="posts")
+
+    def __str__(self):
+        return f"{self.title}, {self.likes} likes"
