fbdb1e6e80ed7a1ded3bbf26843dcd05b65762b1
Rodion Bykov <rodionbykov@gmail.com>
2022-11-15 09:05:00 +0100

    POST method

--- fastapi-realpython/main.py
+++ fastapi-realpython/main.py
@@ -1,26 +1,42 @@
 from fastapi import FastAPI, HTTPException
+from mongita import MongitaClientDisk
+from pydantic import BaseModel
+
+class Animal(BaseModel):
+    id: int
+    kind: str
+    name: str
 
 app = FastAPI()
 
-animals = [
-    {"id": 1, "kind": "Cat", "name": "Felix"},
-    {"id": 2, "kind": "Dog", "name": "Spike"},
-    {"id": 3, "kind": "Raccoon", "name": "Ralph"}
-]
+client = MongitaClientDisk()
+db = client.db
+animals = db.animals
+
+# animals.insert_one({"id": 1, "kind": "Cat", "name": "Felix"})
+# animals.insert_one({"id": 2, "kind": "Dog", "name": "Spike"})
+# animals.insert_one({"id": 3, "kind": "Raccoon", "name": "Ralph"})
 
 @app.get("/")
 async def get_root():
     return {"message": "Hello, world!"}
 
-
 @app.get("/animals")
 async def get_animals():
-    return animals
+    our_animals = animals.find({})
+    return [
+        {key:animal[key] for key in animal if key != "_id"}
+        for animal in our_animals
+    ]
 
 @app.get("/animals/{animal_id}")
 async def get_animal_by_id(animal_id: int):
-    for animal in animals:
-        if animal["id"] == animal_id:
-            return animal
+    if animals.count_documents({"id": animal_id}) > 0:
+        animal = animals.find_one({"id": animal_id})
+        return {key:animal[key] for key in animal if key != "_id"}
     raise HTTPException(status_code=404, detail=f"No animal with id {animal_id}")
 
+@app.post("/animals")
+async def add_animal(new_animal: Animal):
+    animals.insert_one(new_animal.dict())
+    return new_animal
