2698a89ec5a7938fd4428ba9dc467f1462efc1e2
Rodion Bykov <rodionbykov@gmail.com>
2022-11-19 08:24:29 +0100

    Delete method

--- fastapi-realpython/main.py
+++ fastapi-realpython/main.py
@@ -40,3 +40,17 @@ async def get_animal_by_id(animal_id: int):
 async def add_animal(new_animal: Animal):
     animals.insert_one(new_animal.dict())
     return new_animal
+
+@app.put("/animals/{animal_id}")
+async def update_animal(animal_id: int, animal:Animal):
+    if animals.count_documents({"id": animal_id}) > 0:
+        animals.replace_one({"id": animal_id}, animal.dict())
+        return animal
+    raise HTTPException(status_code=404, detail=f"No animal with id {animal_id}")
+
+@app.delete("/animals/{animal_id}")
+async def delete_animal(animal_id: int):
+    delete_result = animals.delete_one({"id": animal_id})
+    if delete_result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail=f"No animal with id {animal_id}")
+    return {"OK": True}
