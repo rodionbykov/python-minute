dda8cd1482affc201690ef3b05a1ceb56c4fc82e
Rodion Bykov <rodionbykov@gmail.com>
2022-12-01 16:04:08 +0100

    Post method introduced

--- url-shortener-realpython/url_shortener_app/main.py
+++ url-shortener-realpython/url_shortener_app/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+import validators
+
+from fastapi import FastAPI, HTTPException
+
+from . import schema
 
 app = FastAPI()
 
 @app.get("/")
 def get_home():
     return "Hello world"
+
+@app.post("/")
+def create_url(url: schema.URLBase):
+    if not validators.url(url.target_url):
+        raise HTTPException(status_code=400, detail="Invalid target URL provided")
+    
+    return "Here be code"
