83c771930a187f586d870be8adefef1a613302aa
Rodion Bykov <rodionbykov@gmail.com>
2023-04-03 10:04:26 +0200

    Relationship added

--- flask-api-realpython/models.py
+++ flask-api-realpython/models.py
@@ -3,9 +3,24 @@
 from datetime import datetime
 from config import db
 
+
+class Note(db.Model):
+    __tablename__ = "notes"
+    id = db.Column(db.Integer, primary_key=True)
+    id_person = db.Column(db.Integer, db.ForeignKey("people.id"))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
 class Person(db.Model):
     __tablename__ = "people"
     id = db.Column(db.Integer, primary_key=True)
     lastname = db.Column(db.String(32), unique=True)
     firstname = db.Column(db.String(32))
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    notes = db.relationship(Note,
+                            backref="people",
+                            cascade="all, delete, delete-orphan",
+                            single_parent=True,
+                            order_by="desc(Note.timestamp)")
