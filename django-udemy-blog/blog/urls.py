5a66143aa6ecaeb65d43c05822c02b35cec7ed60
Rodion Bykov <rodionbykov@gmail.com>
2024-03-06 19:44:02 +0100

    Saved posts feature

--- django-udemy-blog/blog/urls.py
+++ django-udemy-blog/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.StartPageView.as_view(), name="index"),
     path("posts", views.PostsView.as_view(), name="posts-page"),
-    path("posts/<slug:slug>", views.SingleView.as_view(), name="post-details-page")
+    path("posts/read-later", views.ReadLaterView.as_view(), name="post-read-later"),
+    path("posts/<slug:slug>", views.SingleView.as_view(), name="post-details-page"),
 ]
--- django-udemy-blog/blog/views.py
+++ django-udemy-blog/blog/views.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView, DetailView
+from django.views.generic import View, ListView, DetailView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Post
@@ -33,11 +33,20 @@ class SingleView(DetailView):
 
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+
+        saved_posts = request.session.get("saved_posts")
+
+        if saved_posts is not None:
+            is_saved_post = post.id in saved_posts
+        else:
+            is_saved_post = False
+
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "is_saved_post": is_saved_post
         }
 
         return render(request, "blog/details.html", context)
@@ -60,3 +69,36 @@ class SingleView(DetailView):
 
         return render(request, "blog/details.html", context)
 
+
+class ReadLaterView(View):
+
+    def get(self, request):
+
+        saved_posts = request.session.get("saved_posts")
+
+        context = {}
+
+        if saved_posts is None or len(saved_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            context["posts"] = Post.objects.filter(id__in=saved_posts)
+            context["has_posts"] = True
+
+        return render(request, "blog/saved.html", context)
+
+    def post(self, request):
+
+        saved_posts = request.session.get("saved_posts")
+
+        if saved_posts is None:
+            saved_posts = []
+
+        post_id = int(request.POST["postid"])
+
+        if post_id not in saved_posts:
+            saved_posts.append(post_id)
+
+        request.session["saved_posts"] = saved_posts
+
+        return HttpResponseRedirect("/")
